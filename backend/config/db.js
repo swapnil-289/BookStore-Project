12ab7d408d683f8f0a65da45b3ee79b3dd67a400
Swapnil Srivastav <117080515+swapnil-289@users.noreply.github.com>
2026-01-09 17:31:16 +0530

    Update database connection string placeholder

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 export const connectDB = async () => {
-    await mongoose.connect('')
+    await mongoose.connect('"Enter Link here"')
         .then(() => console.log("DB CONNECTED"));
 }
 
