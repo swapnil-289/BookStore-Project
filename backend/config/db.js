77b1b91e9040fba6d4973fccb458a8e892a06822
Swapnil Srivastav <117080515+swapnil-289@users.noreply.github.com>
2026-01-09 17:30:38 +0530

    Update database connection logic
    Remove hardcoded MongoDB connection string.

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 export const connectDB = async () => {
-    await mongoose.connect('mongodb+srv://srivastavswapnil012:KRGk5HagpVr08qcb@mernbookstore.a53jikn.mongodb.net/')
+    await mongoose.connect('')
         .then(() => console.log("DB CONNECTED"));
 }
 
